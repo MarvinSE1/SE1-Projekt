85c63b48775a6a906f6a604333d4293aa798d98e
jonas <jonas@192.168.0.59>
2021-06-22 15:27:32 +0200

    First UI test.

--- test/src/GUI/test.java
+++ test/src/GUI/test.java
@@ -1,5 +1,20 @@
 package GUI;
 
+import javax.swing.*;
+
 public class test {
 
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		
+		JButton b = new JButton("Hello");
+		b.setBounds(130,100,100,40);
+		
+		f.add(b);
+		
+		f.setSize(400,400);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+	
 }
