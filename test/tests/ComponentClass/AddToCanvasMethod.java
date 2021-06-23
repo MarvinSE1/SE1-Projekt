5d1279ae4506dd11da0ffafc8dd37e3c74234820
Linus <2022427@stud.hs-mannheim.de>
2021-06-23 08:24:08 +0200

    tests folder

---
+++ test/tests/ComponentClass/AddToCanvasMethod.java
@@ -0,0 +1,14 @@
+package ComponentClass;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddToCanvasMethod {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
