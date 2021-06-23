9e5c9eaf5f5c1e434cc1829ddbd7551781eef34d
dome6 <dome6@DESKTOP-MCGN94V>
2021-06-23 12:25:52 +0200

    Test gui

--- test/src/GUI/starterWindow.java
+++ test/src/GUI/starterWindow.java
@@ -2,27 +2,30 @@ package GUI;
 
 import javax.swing.*;
 
-public class starterWindow {
+public class starterWindow extends JFrame {
 
 	public static void main(String[] args) {
 		
-		JFrame startWindow = new JFrame();
-		
+		JFrame startWindow = new JFrame("DigitalSim");
+		JPanel start=new JPanel();
+		start.setLayout(new BorderLayout());
 		JButton openFile = new JButton("Open File");
 		JButton newProject = new JButton("New Project");
-		JButton close = new JButton("Close");
+		JButton close = new JButton("Exit");
 		
 		openFile.setBounds(130,50,100,40);
 		newProject.setBounds(130,100,100,40);
 		close.setBounds(130,150,100,40);
 		
+		
 		startWindow.add(openFile);
 		startWindow.add(newProject);
 		startWindow.add(close);
 		
-		startWindow.setSize(400,400);
+		
 		startWindow.setLayout(null);
 		startWindow.setVisible(true);
+		
 	}
 	
 	
