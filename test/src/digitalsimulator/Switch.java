ccb7894554b86962532ccff77ffe9d80521506b3
Anwender <Anwender@DESKTOP-BJ6KFT4.fritz.box>
2021-06-24 21:12:05 +0200

    Switch fast fertig

--- test/src/digitalsimulator/Switch.java
+++ test/src/digitalsimulator/Switch.java
@@ -1,7 +1,33 @@
 package digitalsimulator;
 
-public class Switch extends Component {
+import java.awt.Color;
+import java.awt.Graphics;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Switch extends Component {
+	
+	private JButton switchButton;
+
+	Switch(){
+		output = new ComponentOutput();
+		switchButton = new JButton();
+		switchButton.setBounds(30,30,40,40);
+		output.setBounds(79,40,20,20);
+		add(switchButton);
+		add(output);
+	}
+	
+	public void paint(Graphics g) {
+		g.setColor(getBackground());
+		g.fillRect(getPositionX(),getPositionY(),getWidth(),getHeigth());
+		g.setColor(Color.black);
+		g.drawString("Switch", 33, 21);
+		g.drawRect(getPositionX(),getPositionY(),getWidth()-1,getHeigth()-1);
+		
+	}
+	
 	public void turnSwitchOff() {
 		output.setValue(false);
 	}
@@ -11,7 +37,18 @@ public class Switch extends Component {
 	}
 
 	public boolean getValueSwitch() {
-		return value;
+		return output.getValue();
 	}
 
+public static void main(String[] args) {
+		
+		JFrame test = new JFrame("t");
+		test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		test.setVisible(true);
+		test.setSize(500,500);
+		Switch t = new Switch();
+		Component t2 = new Component();
+		test.add(t);
+		test.add(t2);
+	}
 }
