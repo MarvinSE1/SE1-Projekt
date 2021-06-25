223b8437b32ac228a5f931ff55203c44b2c2144c
Enes <Enes@DESKTOP-6GE8NR4.speedport.ip>
2021-06-25 21:32:49 +0200

    kommentar input hinzugefügt

--- test/src/digitalsimulator/ComponentInput.java
+++ test/src/digitalsimulator/ComponentInput.java
@@ -6,19 +6,26 @@ public class ComponentInput extends JButton {
 
 	private boolean isOnConnection = false;
 	private boolean value;
-	
+
 	public boolean getConnectionStatus() {
 		return isOnConnection;
 	}
-	
+
 	public void setConnectionStatus(boolean status) {
 		isOnConnection = status;
 	}
-	
+
 	public boolean getValue() {
 		return value;
 	}
+
 	public void setValue(boolean newValue) {
 		value = newValue;
 	}
+
+//	public void setValue(Connection connection) {
+//        if (getConnectionStatus() == false)
+//            setConnectionStatus(true);
+//        value = connection.getValue();
+//    }
 }
